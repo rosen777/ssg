09925400647e899b3309d4b9ffb08ff99d68bee1
Rosen Toshev <rtoscheff@gmail.com>
2025-03-13 15:37:35 +0000

    first commit

---
+++ build.js
@@ -0,0 +1,34 @@
+import { renderToStaticMarkup } from "react-dom/server";
+import { createElement as h } from "react";
+import {
+  readFileSync,
+  writeFileSync,
+  existsSync,
+  mkdirSync,
+  readdirSync,
+  unlinkSync,
+} from "node:fs";
+import { fileURLToPath } from "node:url";
+import path, { dirname } from "node:path";
+import App from "./App.js";
+
+const __filname = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filname);
+const distPath = path.join(__dirname, "dist");
+
+const shell = readFileSync(path.join(__dirname, "index.html"), "utf8");
+
+const app = renderToStaticMarkup(h(App));
+
+const html = shell.replace("<!--ROOT-->", app);
+
+if (!existsSync(distPath)) {
+  mkdirSync(distPath);
+} else {
+  const files = readdirSync(distPath);
+  for (const file of files) {
+    unlinkSync(path.join(distPath, file));
+  }
+}
+
+writeFileSync(path.join(distPath, "index.html"), html);
